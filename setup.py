5599d8c837f0bfbc36ef94335d901d4d8f408e9b
Tran Nguyen <trandnnguyen@gmail.com>
2019-08-30 12:49:23 -0700

    changed dependencies

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='galileo',
     version='0.0.4',
+    license='MIT',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
@@ -12,9 +13,13 @@ setup(
     python_requires='>=3.6.7',
     install_requires=[
         'requests>=2.21.0',
-        'sphinx>=2.2.0',
-        'sphinx_rtd_theme>=0.4.3',
-        'python-engineio @ git+ssh://git@github.com/Hyperdyne/python-engineio.git',
-        'python-socketio @ git+ssh://git@github.com/Hyperdyne/python-socketio.git'
+        'python-engineio @ git+ssh://git@github.com/Hyperdyne/python-engineio.git@ef4f74eec7c45690aea6f3d3dab669305b8344ed',
+        'python-socketio @ git+ssh://git@github.com/Hyperdyne/python-socketio.git@5f10992c9ce0a9794abc796412b7792825a4641d'
     ],
+    extras_require={
+        "docs": [
+            'sphinx>=2.2.0',
+            'sphinx_rtd_theme>=0.4.3'
+        ]
+    }
 )
