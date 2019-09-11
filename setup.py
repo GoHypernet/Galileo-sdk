b57f6d9cf0081130bfbe8d3d1b3f49f428330559
Conrad Bailey <conradbailey92@gmail.com>
2019-09-10 17:24:58 -0700

    added readline dependency for Windows

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
+import platform
 from setuptools import setup, find_packages
+
+install_requires = [
+    'requests>=2.21.0',
+    'galileo-socketio',
+]
+if platform.system() == 'Windows':
+    install_requires.extend([
+        'pyreadline',
+    ])
 
 setup(
     name='galileo-sdk',
-    version='0.0.2',
+    version='0.0.3',
     license='MIT',
     packages=find_packages(),
     entry_points={
@@ -11,10 +21,7 @@ setup(
         ]
     },
     python_requires='>=3.6.7',
-    install_requires=[
-        'requests>=2.21.0',
-        'galileo-socketio',
-    ],
+    install_requires=install_requires,
     extras_require={
         "docs": [
             'sphinx>=2.2.0',
