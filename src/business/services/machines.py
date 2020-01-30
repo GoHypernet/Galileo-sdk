9384b43b81b32c00e8eb35c5ed6b0484c5a2b015
Tran Nguyen <trandnnguyen@gmail.com>
2020-01-30 14:57:20 -0800

    Wired machines repo to machines service, added unittests for machines repo and machines service

--- src/business/services/machines.py
+++ src/business/services/machines.py
@@ -1,3 +1,27 @@
+from typing import List, Optional
+from ...data.repositories.machines import MachinesRepository
+
+
 class MachinesService:
-    def __init__(self, machines_repo):
+    def __init__(self, machines_repo: MachinesRepository):
         self._machines_repo = machines_repo
+
+    def get_machine_by_id(self, machine_id: str):
+        r = self._machines_repo.get_machine_by_id(machine_id)
+        return r.json()
+
+    def list_machines(
+        self,
+        mids: Optional[List[str]] = None,
+        userids: Optional[List[str]] = None,
+        page: Optional[int] = 1,
+        items: Optional[int] = 25,
+    ):
+        r = self._machines_repo.list_machines(
+            mids=mids, userids=userids, page=page, items=items
+        )
+        return r.json()
+
+    def update_max_concurrent_jobs(self, mid: str, amount: int):
+        r = self._machines_repo.update_max_concurrent_jobs(mid, amount)
+        return r.status_code == 200
---
+++ tests/unit/data/test_machines_repo.py
@@ -0,0 +1,81 @@
+from unittest import mock
+
+from src.data.repositories.machines import MachinesRepository
+from src.mock_response import MockResponse
+
+BACKEND = "http://BACKEND"
+MID = "machines_id"
+AMOUNT = 10
+
+# Arrange
+settings_repo = mock.Mock()
+settings_repo.get_settings().backend = f"{BACKEND}"
+auth_provider = mock.Mock()
+auth_provider.get_access_token.return_value = "ACCESS_TOKEN"
+machines_repo = MachinesRepository(settings_repo, auth_provider)
+
+
+def mocked_requests_get(*args, **kwargs):
+    if args[0] == f"{BACKEND}/machines/{MID}":
+        return MockResponse({"machine_info": "machine_info"}, 200)
+    elif args[0] == f"{BACKEND}/machines":
+        return MockResponse({"machines": [{"machine": i} for i in range(10)]}, 200)
+    return MockResponse(None, 404)
+
+
+def mocked_requests_put(*args, **kwargs):
+    if args[0] == f"{BACKEND}/machines/{MID}/update_max":
+        return MockResponse(True, 200)
+
+    return MockResponse(None, 404)
+
+
+@mock.patch("requests.get", side_effect=mocked_requests_get)
+def get_machine_by_id(mocked_requests):
+    # Call
+    r = machines_repo.get_machine_by_id(MID)
+    r = r.json()
+
+    # Act
+    mocked_requests.assert_called_once_with(
+        f"{BACKEND}/machines/{MID}",
+        headers={"Authorization": f"Bearer ACCESS_TOKEN"},
+        json=None,
+    )
+
+    # Assert
+    assert r["machine_info"] == "machine_info"
+
+
+@mock.patch("requests.get", side_effect=mocked_requests_get)
+def list_machines(mocked_requests):
+    # Call
+    r = machines_repo.list_machines()
+
+    # Act
+    mocked_requests.assert_called_once_with(
+        f"{BACKEND}/machines",
+        headers={"Authorization": f"Bearer ACCESS_TOKEN"},
+        json={"page": None, "items": None, "mids": None, "userids": None},
+    )
+
+    # Assert
+    assert len(r["machines"]) == 10
+    assert r["machines"][9] == {"machine": 9}
+
+
+@mock.patch("requests.put", side_effect=mocked_requests_put)
+def update_max_concurrent_jobs(mocked_requests):
+    # Call
+    r = machines_repo.update_max_concurrent_jobs(MID, AMOUNT)
+
+    # Act
+    mocked_requests.assert_called_once_with(
+        f"{BACKEND}/machines/{MID}/update_max",
+        headers={"Authorization": f"Bearer ACCESS_TOKEN"},
+        json={"amount": AMOUNT},
+    )
+
+    # Assert
+    assert isinstance(r, bool)
+    assert r == True
