cdfb6fba9f5349af5ffe8c29abe1d61bb375b1fc
Todd Chapman <todd@hypernetlabs.io>
2021-07-11 14:26:52 -0700

    added favicon to docs

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -105,6 +105,8 @@ html_sidebars = {"**": ["globaltoc.html"]}
 
 html_logo = "images/galileo-logo.png"
 
+html_favicon = "images/galileo-icon.png"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
