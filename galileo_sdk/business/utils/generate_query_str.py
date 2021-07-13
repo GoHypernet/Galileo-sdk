8a47e7379138b935bb7035b185eac84c7d7e1c81
Peter Chacko <workpraytoday@gmail.com>
2021-07-13 08:40:55 -0500

    Convert boolean params to lowercase

--- galileo_sdk/business/utils/generate_query_str.py
+++ galileo_sdk/business/utils/generate_query_str.py
@@ -7,10 +7,15 @@ def generate_query_str(*args, **kwargs):
                 pass
             elif isinstance(params[key], list):
                 for list_item in params[key]:
+                    if type(list_item) == bool:
+                        list_item = str(list_item).lower()
                     param_str += "{key}={list_item}&".format(
                         key=key, list_item=list_item
                     )
             else:
+                value = params[key]
+                if type(value) == bool:
+                    value = str(value).lower()
                 param_str += "{key}={param}&".format(key=key, param=params[key])
 
     param_str = param_str[:-1]
