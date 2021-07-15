7236088d07259d6d7ec2afab9da05853d98f7a7d
unknown <todd@hypernetlabs.io>
2021-07-14 20:02:40 -0700

    try to fix html logo

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -103,13 +103,13 @@ html_theme_options = {
 
 html_sidebars = {"**": ["globaltoc.html"]}
 
-html_logo = "images/galileo-logo.png"
+html_logo = "_static/galileo-logo.png"
 
-html_favicon = "images/galileo-icon.png"
+html_favicon = "_static/galileo-icon.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
+html_static_path = ["_static"]
 
 keep_warnings = False
